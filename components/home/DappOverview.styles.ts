6495c429528ace1481d1ac19879747159a022dcf
Kari <evergreenkary@gmail.com>
2023-03-26 23:14:28 +0800

    its almost timeee for the submission aaaahhhh

--- components/home/DappOverview.styles.ts
+++ components/home/DappOverview.styles.ts
@@ -1,83 +1,107 @@
 import styled from "styled-components";
 
-export const VoteWrapper = styled.div`
-  border: 1px solid #e15052;
-  border-radius: 15px;
+export const MainContainer = styled.div`
+	display: flex;
+	flex-direction: column;
 
-  width: 30%;
-  padding: 3em;
-  margin: auto;
+	align-items: center;
+	justify-content: center;
+`;
+
+export const VoteWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: min-content;
+	border: 1px solid #e15052;
+	border-radius: 15px;
+
+	padding: 3em;
+	margin: auto;
 `;
 
 export const IconWrapper = styled.div`
-  justify-content: center;
-  background: #414141;
-  width: 26px;
-  height: 26px;
-  display: flex;
-  align-items: center;
-  border-radius: 10px;
-  margin: auto;
+	justify-content: center;
+	background: #414141;
+	width: 26px;
+	height: 26px;
+	display: flex;
+	align-items: center;
+	border-radius: 10px;
+	margin: auto;
 `;
 
 export const InformationColumn = styled.div`
-  display: flex;
-  margin: 3em 0;
-  font-size: larger;
-  justify-content: center;
-  & > b {
-    color: #8b949e;
-  }
+	display: flex;
+	margin: 3em 0;
+	font-size: larger;
+	justify-content: center;
+	& > b {
+		color: #8b949e;
+	}
 `;
 
 export const NameInputStyle = styled.input`
-  width: 23em;
-  all: unset;
-  height: 1em;
-  margin: 0.5em;
-  padding: 1em;
-  border-radius: 10px;
-  font-size: larger;
-  background: black;
-  border-color: white;
+	width: 23em;
+	all: unset;
+	height: 1em;
+	margin: 0.5em;
+	padding: 1em;
+	/* border: 1px solid green; */
+	border-radius: 10px;
+	font-size: larger;
+	background: black;
+	border-color: white;
 `;
 
 export const SwapBox = styled.div`
-  display: flex;
-  margin: 1em 0;
-  align-items: center;
-  border-radius: 78px;
-  justify-content: center;
-  width: 100%;
-  background: #000000;
-  border: 1px solid rgb(224 80 81);
+	display: flex;
+	margin: 1em 0;
+	align-items: center;
+	border-radius: 78px;
+	justify-content: center;
+	width: max-content;
+	padding: 0 0.5em;
+	background: #000000;
+	border: 1px solid rgb(224 80 81);
 `;
 
 export const TokenSelectStyle = styled.select`
-  height: 3em;
+	height: 3em;
 
-  cursor: pointer;
-  background: #1b1b1b;
-  border-radius: 32px;
-  padding: 0 30px;
-  border: black;
-  font-weight: 800;
-  font-size: inherit;
+	cursor: pointer;
+	background: #1b1b1b;
+	border-radius: 32px;
+	padding: 0 30px;
+	border: black;
+	font-weight: 800;
+	font-size: inherit;
 `;
 
 export const SwapButton = styled.div`
-  background: linear-gradient(
-    131deg,
-    rgb(239 118 160) 26%,
-    rgb(239 102 138) 52%,
-    rgb(224 80 81) 92%
-  );
-  padding: 1em 10em;
-  border-radius: 78px;
-  font-size: large;
-  font-weight: 800;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
+	background: linear-gradient(
+		131deg,
+		rgb(239 118 160) 26%,
+		rgb(239 102 138) 52%,
+		rgb(224 80 81) 92%
+	);
+	padding: 1em 10em;
+	border-radius: 78px;
+	font-weight: 800;
+	display: flex;
+	width: 100%;
+	align-items: center;
+	justify-content: center;
+	cursor: pointer;
+`;
+
+export const SubscribeBox = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+
+	margin-top: 4rem;
+	font-size: 0.9rem;
+	font-weight: 600;
 `;
--- components/home/DappOverview.tsx
+++ components/home/DappOverview.tsx
@@ -1,89 +1,218 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import * as S from "./DappOverview.styles";
 import { BsCaretDownFill } from "react-icons/bs";
 import { Provider, Wallet } from "zksync-web3";
-import { ethers } from "ethers";
+import { BigNumber, ethers, utils } from "ethers";
 import { protocolATotal } from "@/shared/abi/protocolATotal";
+import { membershipTotal } from "@/shared/abi/membershipTotal";
+import styled from "styled-components";
 
 const DappOverview = () => {
-  const [swapValue, setSwapValue] = useState<number>(0);
-  const [select1Value, setSelect1Value] = useState<string>("FUCH");
-  const [select2Value, setSelect2Value] = useState<string>("SIA");
-  const handleValue = (e: any) => {
-    const value = e.target.value;
-    setSwapValue(value);
-  };
-  const handleSelect1 = (e: any) => {
-    const value = e.target.value;
-    setSelect1Value(value);
-    if (value === "FUCH") {
-      setSelect2Value("SIA");
-    } else {
-      setSelect2Value("FUCH");
-    }
-  };
-  const handleSelect2 = (e: any) => {
-    const value = e.target.value;
-    setSelect2Value(value);
-    if (value === "FUCH") {
-      setSelect1Value("SIA");
-    } else {
-      setSelect1Value("FUCH");
-    }
-  };
-  const wallet = Wallet.createRandom();
-  const handleSwap = () => {
-    const provider = new Provider("https://testnet.era.zksync.dev");
-    const zksyncWallet = wallet.connect(provider);
+	const [swapValue, setSwapValue] = useState<number>(0);
+	const [select1Value, setSelect1Value] = useState<string>("FUCH");
+	const [select2Value, setSelect2Value] = useState<string>("SIA");
 
-    const protocolContract = new ethers.Contract(
-      "0x8e74FbeE22c3B77B00447e71fFc0A45d68761785",
-      protocolATotal.abi,
-      zksyncWallet
-    );
-    console.log(protocolContract);
-  };
-  return (
-    <>
-      <S.InformationColumn>
-        <h1>Amogusswap</h1>
-      </S.InformationColumn>
-      <S.VoteWrapper>
-        <h3>{select1Value === "FUCH" ? "$FUCH -> $SIA" : "$SIA -> $FUCH"}</h3>
+	const [account, setAccount] = useState<string | null>(null);
 
-        <div>
-          <S.SwapBox>
-            <S.NameInputStyle
-              placeholder="0"
-              type="number"
-              value={swapValue}
-              onChange={handleValue}
-            />
-            <S.TokenSelectStyle onChange={handleSelect1} value={select1Value}>
-              <option value="FUCH">FUCH</option>
-              <option value="SIA">SIA</option>
-            </S.TokenSelectStyle>
-          </S.SwapBox>
-          <S.IconWrapper>
-            <BsCaretDownFill />
-          </S.IconWrapper>
-          <S.SwapBox>
-            <S.NameInputStyle
-              placeholder="0"
-              type="number"
-              value={swapValue}
-              onChange={handleValue}
-            />
-            <S.TokenSelectStyle onChange={handleSelect2} value={select2Value}>
-              <option value="FUCH">FUCH</option>
-              <option value="SIA">SIA</option>
-            </S.TokenSelectStyle>
-          </S.SwapBox>
-        </div>
-        <S.SwapButton onClick={handleSwap}>Swap</S.SwapButton>
-      </S.VoteWrapper>
-    </>
-  );
+	const [tier, setTier] = useState(0);
+
+	const provider = useMemo(() => {
+		return new Provider("https://zksync2-testnet.zksync.dev");
+	}, []);
+
+	const protocol = useMemo(() => {
+		return new ethers.Contract(
+			"0x8e74FbeE22c3B77B00447e71fFc0A45d68761785",
+			protocolATotal.abi,
+			provider
+		);
+	}, []);
+
+	useEffect(() => {
+		if (!account) return;
+
+		const c = new ethers.Contract(
+			"0x4d69de6Ce6EdDb5F35D6549A25332Ff15D718FCB",
+			membershipTotal.abi,
+			provider
+		);
+
+		c.userTier(account)
+			// @ts-ignore
+			.then((value) => setTier(tier))
+			.catch(console.log);
+	}, []);
+
+	const handleSubscribe = async () => {
+		let tx = await protocol.populateTransaction.swapExactTokenAWithTokenB(69420);
+
+		try {
+			//@ts-ignore
+			await window.ethereum.request({
+				//@ts-ignore
+				method: "eth_sendTransaction",
+				params: [
+					// {
+					// 	// @ts-ignore
+					// 	from: account,
+					// 	to: protocol.address,
+					// 	gas: "0x76c0", // 30400
+					// 	gasPrice: "0x9184e72a000", // 10000000000000
+					// 	value: "0", // 2441406250
+					// 	data: "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675",
+					// },
+					{
+						...tx,
+						// @ts-ignore
+						from: account,
+						gasPrice: BigNumber.from("0x9184e72a000"), // 10000000000000
+					},
+				],
+			});
+		} catch (e) {
+			console.log("User rejected");
+		}
+	};
+
+	const handleValue = (e: any) => {
+		const value = e.target.value;
+		setSwapValue(value);
+	};
+	const handleSelect1 = (e: any) => {
+		const value = e.target.value;
+		setSelect1Value(value);
+		if (value === "FUCH") {
+			setSelect2Value("SIA");
+		} else {
+			setSelect2Value("FUCH");
+		}
+	};
+	const handleSelect2 = (e: any) => {
+		const value = e.target.value;
+		setSelect2Value(value);
+		if (value === "FUCH") {
+			setSelect1Value("SIA");
+		} else {
+			setSelect1Value("FUCH");
+		}
+	};
+
+	const handleSwap = async () => {
+		if (!account) return;
+
+		let tx = await protocol.populateTransaction.swapExactTokenAWithTokenB(69420);
+
+		try {
+			//@ts-ignore
+			await window.ethereum.request({
+				//@ts-ignore
+				method: "eth_sendTransaction",
+				params: [
+					// {
+					// 	// @ts-ignore
+					// 	from: account,
+					// 	to: protocol.address,
+					// 	gas: "0x76c0", // 30400
+					// 	gasPrice: "0x9184e72a000", // 10000000000000
+					// 	value: "0", // 2441406250
+					// 	data: "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675",
+					// },
+					{
+						...tx,
+						// @ts-ignore
+						from: account,
+						gasPrice: BigNumber.from("0x9184e72a000"), // 10000000000000
+					},
+				],
+			});
+		} catch (e) {
+			console.log("User rejected");
+		}
+	};
+	return (
+		<>
+			<S.InformationColumn>
+				<h1>Umiswap</h1>
+			</S.InformationColumn>
+			<S.MainContainer>
+				<S.VoteWrapper>
+					<h3>{select1Value === "FUCH" ? "$FUCH -> $SIA" : "$SIA -> $FUCH"}</h3>
+
+					<div>
+						<S.SwapBox>
+							<S.NameInputStyle
+								placeholder="0"
+								type="number"
+								value={swapValue}
+								onChange={handleValue}
+							/>
+							<S.TokenSelectStyle
+								onChange={handleSelect1}
+								value={select1Value}
+							>
+								<option value="FUCH">FUCH</option>
+								<option value="SIA">SIA</option>
+							</S.TokenSelectStyle>
+						</S.SwapBox>
+						<S.IconWrapper>
+							<BsCaretDownFill />
+						</S.IconWrapper>
+						<S.SwapBox>
+							<S.NameInputStyle
+								placeholder="0"
+								type="number"
+								value={swapValue}
+								onChange={handleValue}
+							/>
+							<S.TokenSelectStyle
+								onChange={handleSelect2}
+								value={select2Value}
+							>
+								<option value="FUCH">FUCH</option>
+								<option value="SIA">SIA</option>
+							</S.TokenSelectStyle>
+						</S.SwapBox>
+					</div>
+
+					{account ? (
+						<S.SwapButton onClick={handleSwap}>Swap</S.SwapButton>
+					) : (
+						<S.SwapButton
+							onClick={async () => {
+								// @ts-ignore
+								const [account] = await window.ethereum.request({
+									method: "eth_requestAccounts",
+								});
+								console.log("account ", account);
+								setAccount(account);
+							}}
+						>
+							Connect
+						</S.SwapButton>
+					)}
+				</S.VoteWrapper>
+
+				{!tier && account && (
+					<S.SubscribeBox>
+						It seems you are not a part of our membership. Consider joining to
+						enjoy exclusive benefits.
+						<S.SwapButton
+							onClick={handleSubscribe}
+							style={{
+								width: "10rem",
+								fontWeight: 400,
+								fontSize: "0.9rem",
+								padding: "0.5rem 0",
+								marginTop: "1rem",
+							}}
+						>
+							Subscribe
+						</S.SwapButton>
+					</S.SubscribeBox>
+				)}
+			</S.MainContainer>
+		</>
+	);
 };
 
 export default DappOverview;
